27b4e34bda941bdab732dfadf573edd0f7cd46f7
uAreASimp <109386339+uAreASimp@users.noreply.github.com>
2024-06-23 21:39:05 +0000

    Update AutoPlay.js v0.2
    Added a context menu for easier song selection.
    -Right click a track to select as the song that autoplays on pause.
    
    Added a text box bottom right when autoplayed
    -A text box will show up in bottom right corner letting the user know that the song currently playing was autoplayed.

--- Extensions/AutoPlay.js
+++ Extensions/AutoPlay.js
@@ -1,45 +1,95 @@
 // NAME: Autoplay
 // AUTHOR: uAreASimp
-// VERSION: 0.1
+// VERSION: 0.2
 // DESCRIPTION: Autoplays selected song after having song be paused for 5 seconds, pause song to revert back to original before autoplay.
+/// <reference path="../../../Local/spicetify/globals.d.ts" />
 
 
 (function AutoPlay() {
 
+    if (!(Spicetify.showNotification && Spicetify.Platform && Spicetify.ContextMenu && Spicetify.URI && Spicetify.SVGIcons)) {
+        setTimeout(AutoPlay, 10);
+        return;
+    }
+
+
     //CHANGE FOR SONG YOU WANT TO AUTOPLAY
-    const SONG_URI = "spotify:track:0MUQtVlIkuMeDfZFm5xKRq"
+    let SONG_URI = localStorage.getItem("SONG_URI") || "spotify:track:4PTG3Z6ehGkBFwjybzWkR8";
+    //CHANGE FOR SONG YOU WANT TO AUTOPLAY
+
+
 
     // Constants
     const BUTTON_NAME_TEXT = "AutoPlay";
     const STORAGE_KEY = "autoplay_spicetify";
+    const AUTO_PLAYED_KEY = "is_auto_played"
+
+
+    // Add CSS for the textbox
+    const style = document.createElement('style');
+    style.innerHTML = `
+    #autoplayIndicator {
+        position: fixed;
+        bottom: 100px; /* Adjust this value as needed */
+        right: 10px;
+        padding: 10px;
+        background-color: rgba(255, 255, 255, 1);
+        color: black;
+        border-radius: 5px;
+        display: none;
+        z-index: 1000;
+    }
+`;
+    document.head.appendChild(style);
+
+    // Add the textbox to the document
+    const indicator = document.createElement('div');
+    indicator.id = 'autoplayIndicator';
+    indicator.innerText = 'Autoplayed, pause to return';
+    document.body.appendChild(indicator);
 
 
 
     // Retrieve or initialize state from localStorage
     let autoPlayVar = localStorage.getItem(STORAGE_KEY) === "true";
-    let autoPlayedVar = localStorage.getItem(STORAGE_KEY) === "false";
-    let savedPlaybackState = null; 
+    let autoPlayedVar = localStorage.getItem(AUTO_PLAYED_KEY) === "true"; // Updated key here
+    let savedPlaybackState = null;
+
+    // Initialize autoPlayedVar if it's not set
+    if (autoPlayedVar === null) {
+        autoPlayedVar = true;
+        localStorage.setItem(AUTO_PLAYED_KEY, true);
+    }
 
     // Reference to the button instance
-    let autoplayButton = null;
+    let autoplayButton;
 
     // Function to toggle autoplay variable and update localStorage
     function toggleAutoPlay() {
         autoPlayVar = !autoPlayVar; // Toggle the variable
         localStorage.setItem(STORAGE_KEY, autoPlayVar); // Save to localStorage
+
+        if (autoPlayedVar) { // Updated logic for autoPlayedVar
+            restorePlaybackState();
+        }
+
         updateButton(); // Update the button's appearance
     }
 
     // Function to update the button's appearance based on autoPlayVar
     function updateButton() {
         const icon = autoPlayVar
-            ? `<svg role="img" height="16" width="16" viewBox="0 0 16 16" fill="currentColor"><path d="M13.350175,0.37457282C9.7802043,0.37457282,6.2102339,0.37457282,2.6402636,0.37457282C2.1901173,0.43000784,2.3537108,0.94911284,2.3229329,1.2621688C2.3229329,5.9446788,2.3229329,10.62721,2.3229329,15.309742C2.4084662,15.861041,2.9630936,15.536253,3.1614158,15.248148C4.7726941,13.696623,6.3839408,12.145098,7.9952191,10.593573C9.7069009,12.241789,11.418583,13.890005,13.130265,15.53822C13.626697,15.863325,13.724086,15.200771,13.667506,14.853516C13.667506,10.132999,13.667506,5.4124518,13.671726,0.52196684C13.520105,0.37034182,13.350175,0.37457282ZM13.032844,14.563698C11.426929,13.017345,9.8210448,11.470993,8.2151293,9.9246401C7.8614008,9.6568761,7.6107412,10.12789,7.3645243,10.320193C5.8955371,11.734694,4.4265815,13.149196,2.9575943,14.563698C2.9575943,10.045543,2.9575943,5.5273888,2.9575943,1.0092338C6.3160002,1.0092338,9.674438,1.0092338,13.032844,1.0092338C13.032844,5.5273888,13.032844,10.045543,13.032844,14.563698Z"></path></svg>`
-            : `<svg role="img" height="16" width="16" viewBox="0 0 16 16" fill="currentColor"><path d="M2,2.5C2,2.22386,2.22386,2,2.5,2H13.5C13.7761,2,14,2.22386,14,2.5C14,2.77614,13.7761,3,13.5,3H2.5C2.22386,3,2,2.77614,2,2.5ZM2.5,7C2.22386,7,2,7.22386,2,7.5C2,7.77614,2.22386,8,2.5,8H13.5C13.7761,8,14,7.77614,14,7.5C14,7.22386,13.7761,7,13.5,7H2.5ZM2,12.5C2,12.2239,2.22386,12,2.5,12H13.5C13.7761,12,14,12.2239,14,12.5C14,12.7761,13.7761,13,13.5,13H2.5C2.22386,13,2,12.7761,2,12.5Z"></path></svg>`;
-
-        const buttonText = autoPlayVar ? `${BUTTON_NAME_TEXT} (On)` : `${BUTTON_NAME_TEXT} (Off)`;
+            ? `<svg role="img" height="16" width="16" viewBox="0 0 16 16" fill="currentColor">
+                    <path d="M3 2l10 6-10 6V2z"></path>
+                  </svg>`
+            : `<svg role="img" height="16" width="16" viewBox="0 0 16 16" fill="currentColor">
+                     <path d="M3 2h3v12H3zm7 0h3v12h-3z"></path>`
+        const buttonText = autoPlayVar
+            ? `${BUTTON_NAME_TEXT} (On)`
+            : `${BUTTON_NAME_TEXT} (Off)`
 
         if (autoplayButton) {
-            autoplayButton.text = buttonText;
+            autoplayButton.label = buttonText;
             autoplayButton.icon = icon;
         } else {
             autoplayButton = new Spicetify.Topbar.Button(
@@ -59,40 +109,52 @@
     let timerId = null;
     let lastPlayerState = null;
 
+    // Function checkPlaybackState() is updated to ensure correct handling of autoPlayedVar
     function checkPlaybackState() {
-
         // Check if autoPlayVar is true
         if (!autoPlayVar) {
-            console.log("Auto play is not enabled. Skipping playback state check.");
+            //console.log("Auto play is not enabled. Skipping playback state check.");
             clearTimer();
             return;
         }
 
-        console.log("Checking playback state...");
+        //console.log("Checking playback state...");
         const playerState = getPlayerState();
 
         if (!playerState) {
-            console.log("No player state available.");
+            //console.log("No player state available.");
             return;
         }
 
         const isPlaying = !playerState.isPaused;
-        console.log("isPlaying:", isPlaying);
+        //console.log("isPlaying:", isPlaying);
 
         if (isPlaying) {
-            console.log("Song is playing. Clearing timer if it exists.");
+            //console.log("Song is playing. Clearing timer if it exists.");
             clearTimer();
             lastPlayerState = playerState;
-        } else {
-            if (autoPlayedVar === true) {
-                restorePlaybackState();
+            if (!autoPlayedVar) {
+                document.getElementById('autoplayIndicator').style.display = 'none';
             }
-            else {
-                console.log("No song playing. Starting timer.");
+        } else {
+            if (autoPlayedVar) { // Updated logic for autoPlayedVar
+                restorePlaybackState();
+            } else {
+                //console.log("No song playing. Starting timer.");
+                startTimer();
+            }
+        }
+
+        if (playerState.isPaused && autoPlayVar) {
+            if (autoPlayedVar) { // Updated logic for autoPlayedVar
+                restorePlaybackState();
+            } else {
+                //console.log("No song playing. Starting timer.");
                 startTimer();
             }
         }
     }
+
 
     function getPlayerState() {
         const player = Spicetify.Player;
@@ -102,8 +164,9 @@
         return null;
     }
 
+    // Function startTimer() is updated to include localStorage update
     function startTimer() {
-        if (timerId === null) {
+        if (timerId === null && autoPlayVar) { // Check if autoPlayVar is true
             timerId = setTimeout(() => {
                 console.log(`No song playing for ${WAIT_TIME} ms. Auto-playing song: ${SONG_URI}`);
                 saveCurrentPlaybackState();
@@ -112,12 +175,16 @@
                     playSong(SONG_URI);
                     Spicetify.Player.setRepeat(2);
                     Spicetify.Player.setShuffle(false);
+                    // Show the autoplay indicator
+                    document.getElementById('autoplayIndicator').style.display = 'block';
                 }, 200);
                 autoPlayedVar = true;
+                localStorage.setItem(AUTO_PLAYED_KEY, true); // Updated localStorage here
                 timerId = null; // Reset timerId after playing the song
             }, WAIT_TIME);
         }
     }
+
 
     function clearTimer() {
         if (timerId !== null) {
@@ -174,257 +241,54 @@
 
 
 
-    function restorePlaybackState() {
-        if (savedPlaybackState) {
-            console.log("Restoring playback state:", savedPlaybackState);
-            Spicetify.Player.playUri(savedPlaybackState.uri);
-            setTimeout(() => {
-                Spicetify.Player.setVolume(savedPlaybackState.volume);
-                Spicetify.Player.setRepeat(savedPlaybackState.repeat);
-                Spicetify.Player.setShuffle(savedPlaybackState.shuffle);
-                Spicetify.Player.seek(savedPlaybackState.position);
-                savedPlaybackState = null; // Clear saved state after restoring
-            }, 500);
-            autoPlayedVar = false;
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    function initialize() {
-        console.log("Initializing script...");
-        if (typeof Spicetify === "undefined" || !Spicetify.Player) {
-            console.log("Spicetify or Spicetify.Player not ready. Retrying initialization in 1 second.");
-            setTimeout(initialize, 1000);
-            return;
-        }
-
-        const player = Spicetify.Player;
-        if (player) {
-            console.log("Spicetify player found. Starting playback state check.");
-            setInterval(checkPlaybackState, 1000); // Check playback state every second
-        } else {
-            console.log("Spicetify player not ready. Retrying initialization in 1 second.");
-            setTimeout(initialize, 1000);
-        }
-    }
-
-    initialize();
-})();
-// NAME: Autoplay
-// AUTHOR: uAreASimp
-// VERSION: 0.1
-// DESCRIPTION: Autoplays selected song after having song be paused for 5 seconds, pause song to revert back to original before autoplay.
-
-
-(function AutoPlay() {
-
-    //CHANGE FOR SONG YOU WANT TO AUTOPLAY
-    const SONG_URI = "spotify:track:0MUQtVlIkuMeDfZFm5xKRq"
-
-    // Constants
-    const BUTTON_NAME_TEXT = "AutoPlay";
-    const STORAGE_KEY = "autoplay_spicetify";
-
-
-
-    // Retrieve or initialize state from localStorage
-    let autoPlayVar = localStorage.getItem(STORAGE_KEY) === "true";
-    let autoPlayedVar = localStorage.getItem(STORAGE_KEY) === "false";
-    let savedPlaybackState = null; 
-
-    // Reference to the button instance
-    let autoplayButton = null;
-
-    // Function to toggle autoplay variable and update localStorage
-    function toggleAutoPlay() {
-        autoPlayVar = !autoPlayVar; // Toggle the variable
-        localStorage.setItem(STORAGE_KEY, autoPlayVar); // Save to localStorage
-        updateButton(); // Update the button's appearance
-    }
-
-    // Function to update the button's appearance based on autoPlayVar
-    function updateButton() {
-        const icon = autoPlayVar
-            ? `<svg role="img" height="16" width="16" viewBox="0 0 16 16" fill="currentColor"><path d="M13.350175,0.37457282C9.7802043,0.37457282,6.2102339,0.37457282,2.6402636,0.37457282C2.1901173,0.43000784,2.3537108,0.94911284,2.3229329,1.2621688C2.3229329,5.9446788,2.3229329,10.62721,2.3229329,15.309742C2.4084662,15.861041,2.9630936,15.536253,3.1614158,15.248148C4.7726941,13.696623,6.3839408,12.145098,7.9952191,10.593573C9.7069009,12.241789,11.418583,13.890005,13.130265,15.53822C13.626697,15.863325,13.724086,15.200771,13.667506,14.853516C13.667506,10.132999,13.667506,5.4124518,13.671726,0.52196684C13.520105,0.37034182,13.350175,0.37457282ZM13.032844,14.563698C11.426929,13.017345,9.8210448,11.470993,8.2151293,9.9246401C7.8614008,9.6568761,7.6107412,10.12789,7.3645243,10.320193C5.8955371,11.734694,4.4265815,13.149196,2.9575943,14.563698C2.9575943,10.045543,2.9575943,5.5273888,2.9575943,1.0092338C6.3160002,1.0092338,9.674438,1.0092338,13.032844,1.0092338C13.032844,5.5273888,13.032844,10.045543,13.032844,14.563698Z"></path></svg>`
-            : `<svg role="img" height="16" width="16" viewBox="0 0 16 16" fill="currentColor"><path d="M2,2.5C2,2.22386,2.22386,2,2.5,2H13.5C13.7761,2,14,2.22386,14,2.5C14,2.77614,13.7761,3,13.5,3H2.5C2.22386,3,2,2.77614,2,2.5ZM2.5,7C2.22386,7,2,7.22386,2,7.5C2,7.77614,2.22386,8,2.5,8H13.5C13.7761,8,14,7.77614,14,7.5C14,7.22386,13.7761,7,13.5,7H2.5ZM2,12.5C2,12.2239,2.22386,12,2.5,12H13.5C13.7761,12,14,12.2239,14,12.5C14,12.7761,13.7761,13,13.5,13H2.5C2.22386,13,2,12.7761,2,12.5Z"></path></svg>`;
-
-        const buttonText = autoPlayVar ? `${BUTTON_NAME_TEXT} (On)` : `${BUTTON_NAME_TEXT} (Off)`;
-
-        if (autoplayButton) {
-            autoplayButton.text = buttonText;
-            autoplayButton.icon = icon;
-        } else {
-            autoplayButton = new Spicetify.Topbar.Button(
-                buttonText,
-                icon,
-                self => {
-                    toggleAutoPlay();
-                }
-            );
-        }
-    }
-
-    // Initialize the button
-    updateButton();
-
-    const WAIT_TIME = 5000; // 5 seconds
-    let timerId = null;
-    let lastPlayerState = null;
-
-    function checkPlaybackState() {
-
-        // Check if autoPlayVar is true
-        if (!autoPlayVar) {
-            console.log("Auto play is not enabled. Skipping playback state check.");
-            clearTimer();
-            return;
-        }
-
-        console.log("Checking playback state...");
-        const playerState = getPlayerState();
-
-        if (!playerState) {
-            console.log("No player state available.");
-            return;
-        }
-
-        const isPlaying = !playerState.isPaused;
-        console.log("isPlaying:", isPlaying);
-
-        if (isPlaying) {
-            console.log("Song is playing. Clearing timer if it exists.");
-            clearTimer();
-            lastPlayerState = playerState;
-        } else {
-            if (autoPlayedVar === true) {
-                restorePlaybackState();
-            }
-            else {
-                console.log("No song playing. Starting timer.");
-                startTimer();
-            }
-        }
-    }
-
-    function getPlayerState() {
-        const player = Spicetify.Player;
-        if (player && player.data) {
-            return player.data;
-        }
-        return null;
-    }
-
-    function startTimer() {
-        if (timerId === null) {
-            timerId = setTimeout(() => {
-                console.log(`No song playing for ${WAIT_TIME} ms. Auto-playing song: ${SONG_URI}`);
-                saveCurrentPlaybackState();
-                Spicetify.Player.setVolume(0);
-                setTimeout(() => {
-                    playSong(SONG_URI);
-                    Spicetify.Player.setRepeat(2);
-                    Spicetify.Player.setShuffle(false);
-                }, 200);
-                autoPlayedVar = true;
-                timerId = null; // Reset timerId after playing the song
-            }, WAIT_TIME);
-        }
-    }
-
-    function clearTimer() {
-        if (timerId !== null) {
-            clearTimeout(timerId);
-            timerId = null;
-        }
-    }
-
-    async function playSong(uri) {
-        console.log("Attempting to play song:", uri);
-        await Spicetify.Player.playUri(uri);
-    }
-
-
-
- function saveCurrentPlaybackState() {
-    const playerState = getPlayerState();
-
-    if (playerState) {
-        const wasPaused = playerState.isPaused;
-        
-        // Resume playback if it's paused
-        if (wasPaused) {
-            Spicetify.Player.play();
-        }
-
-        // Save the volume directly
-        savedPlaybackState = {
-            uri: playerState.progress !== undefined ? playerState.progress : Spicetify.Player.data?.item.uri,
-            position: playerState.progress !== undefined ? playerState.progress : Spicetify.Player.getProgressPercent(),
-            volume: playerState.volume !== undefined ? playerState.volume : Spicetify.Player.getVolume(),
-            repeat: Spicetify.Player.getRepeat(),
-            shuffle: Spicetify.Player.getShuffle(),
-        };
-
-        
-        //console.log("Current track URI:", Spicetify.Player.data?.item.uri);
-
-        console.log("Saved playback URI:", savedPlaybackState.uri);
-        console.log("Saved playback progress:", savedPlaybackState.position);
-        console.log("Saved playback volume:", savedPlaybackState.volume);
-        console.log("Saved playback repeat:", savedPlaybackState.repeat);
-        console.log("Saved playback shuffle:", savedPlaybackState.shuffle);
-    } else {
-        console.log("No player state available. Cannot save playback volume.");
+    // Function restorePlaybackState() is updated to include localStorage update
+function restorePlaybackState() {
+    if (savedPlaybackState && autoPlayedVar) { // Check if autoPlayedVar is true
+        console.log("Restoring playback state:", savedPlaybackState);
+        Spicetify.Player.playUri(savedPlaybackState.uri);
+        setTimeout(() => {
+            Spicetify.Player.setVolume(savedPlaybackState.volume);
+            Spicetify.Player.setRepeat(savedPlaybackState.repeat);
+            Spicetify.Player.setShuffle(savedPlaybackState.shuffle);
+            Spicetify.Player.seek(savedPlaybackState.position);
+            savedPlaybackState = null; // Clear saved state after restoring
+            // Hide the autoplay indicator
+            document.getElementById('autoplayIndicator').style.display = 'none';
+        }, 500);
+        autoPlayedVar = false;
+        localStorage.setItem(AUTO_PLAYED_KEY, false); // Updated localStorage here
     }
 }
 
 
+    const { Type } = Spicetify.URI;
 
-
-
-
-
-
-
-    function restorePlaybackState() {
-        if (savedPlaybackState) {
-            console.log("Restoring playback state:", savedPlaybackState);
-            Spicetify.Player.playUri(savedPlaybackState.uri);
-            setTimeout(() => {
-                Spicetify.Player.setVolume(savedPlaybackState.volume);
-                Spicetify.Player.setRepeat(savedPlaybackState.repeat);
-                Spicetify.Player.setShuffle(savedPlaybackState.shuffle);
-                Spicetify.Player.seek(savedPlaybackState.position);
-                savedPlaybackState = null; // Clear saved state after restoring
-            }, 500);
-            autoPlayedVar = false;
-        }
+    function setTrackAsAuto(uri) {
+        let uriObj = Spicetify.URI.fromString(uri[0]);
+        localStorage.setItem("SONG_URI", "spotify:track:" + uriObj.id);
+        SONG_URI = localStorage.getItem("SONG_URI") || "spotify:track:4PTG3Z6ehGkBFwjybzWkR8";
+        Spicetify.showNotification("Song selected for AutoPlay."),
+        console.log("URI Object:", "spotify:track:" + uriObj.id);
     }
 
 
+    function ifItemIsTrack(uri) {
+        let uriObj = Spicetify.URI.fromString(uri[0]);
+        switch (uriObj.type) {
+            case Type.TRACK:
+                return true;
+        }
+        return false;
+    }
 
-
-
-
-
-
-
+    // Create a new menu item that only appears when a track is selected
+    const menuItem = new Spicetify.ContextMenu.Item(
+        "Set as Autoplay",
+        setTrackAsAuto,
+        ifItemIsTrack,
+        Spicetify.SVGIcons["play"],
+        false,
+    ).register();
 
 
 
